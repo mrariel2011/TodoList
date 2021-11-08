2571ae52b97e56d1da41bd4ab3e0ebaeebd56d17
Roger Camargo <huogerac@gmail.com>
2021-11-08 16:47:39 -0300

    Adiciona bootstrap

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 from django.urls import path, include
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/tasks')),
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls')),
 ]
