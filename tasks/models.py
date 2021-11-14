29cb3bc1bc231cbb872cccac7bc34eb9462abf9c
Roger Camargo <huogerac@gmail.com>
2021-11-13 21:43:21 -0300

    Adiciona tabela de tarefas

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+
+    title = models.CharField(max_length=264)
+    description = models.TextField(null=True, blank=True)
+    due_to = models.DateTimeField()
+    done = models.BooleanField(default=False)
--- tasks/views.py
+++ tasks/views.py
@@ -1,20 +1,12 @@
 from django.views.generic import TemplateView
 
+from .models import Task
+
 
 class ListTasksView(TemplateView):
     template_name = "tasks/lists.html"
 
     def get_context_data(self, **kwargs):
         context = super(ListTasksView, self).get_context_data(**kwargs)
-        context["tasks"] = [
-            {
-                "id": 1,
-                "title": "Task 1",
-            },
-            {
-                "id": 2,
-                "title": "Task 2",
-            },
-            {"id": 3, "title": "Task 3", "done": True},
-        ]
+        context["tasks"] = Task.objects.filter(done=False)
         return context
