f24e11e5f4a1aef5ef9539e4c2b31fbfefa28b90
Ariel <mrariel2011@hotmail.com>
2021-11-15 12:31:50 -0300

    Linkando home/lists pages
    Essa coisa deu trabalho que não devia, ta louco

--- todolist/urls.py
+++ todolist/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     # path("", RedirectView.as_view(url="/tasks")),
     path("admin/", admin.site.urls),
-    path("homepage/", include("homepage.urls")),
+    path("", include("homepage.urls")),
     path("tasks/", include("tasks.urls")),
 ]
 
