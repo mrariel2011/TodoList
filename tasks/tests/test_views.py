080c14f69fbe7156a764182343e76ee62c63e8f6
Roger Camargo <huogerac@gmail.com>
2021-11-21 16:17:01 -0300

    fix pytest

--- tasks/tests/test_views.py
+++ tasks/tests/test_views.py
@@ -1,9 +1,7 @@
-
 # Create your tests here.
-def test_something(client):
-    #response = client.get('/some_url_defined_in_test_urls/').content
-    response = client.get('/tasks/')
+def test_something(client, db):
+    # response = client.get('/some_url_defined_in_test_urls/').content
+    response = client.get("/tasks/")
 
     assert response.status_code == 200
-    assert 'tasks list' in response.content.decode("utf-8") 
-
+    assert "Tasks list" in response.content.decode("utf-8")
