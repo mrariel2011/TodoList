af1951243ae3692aa78133f156e1a134f71f6dae
Roger Camargo <huogerac@gmail.com>
2021-11-21 15:10:38 -0300

    Remove o warnings de uso errado de data default

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,10 @@
-from datetime import datetime
 from django.db import models
+from django.utils import timezone
 
 
 class Task(models.Model):
 
     title = models.CharField(max_length=264)
     description = models.TextField(null=True, blank=True)
-    due_to = models.DateTimeField(default=datetime.now())
+    due_to = models.DateTimeField(default=timezone.now)
     done = models.BooleanField(default=False)
