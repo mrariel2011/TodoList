89ba0ff037eb0c57f32e07dc1fd9c77d76af07ff
Ariel <mrariel2011@hotmail.com>
2021-12-06 21:17:15 -0300

    Controle Usuario

--- tasks/views.py
+++ tasks/views.py
@@ -4,12 +4,16 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import reverse
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 from .models import Task
 from .forms import TaskModelForm
 
 
-class TaskUpdateView(UpdateView):
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy("usuarios.login")
     model = Task
     form_class = TaskModelForm
     context_object_name = "task"
@@ -21,7 +25,8 @@ class TaskUpdateView(UpdateView):
         return reverse("tasks.list")
 
 
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy("usuarios.login")
     model = Task
     form_class = TaskModelForm
     context_object_name = "task"
@@ -33,7 +38,8 @@ class TaskDeleteView(DeleteView):
         return reverse("tasks.list")
 
 
-class TaskInsertView(CreateView):
+class TaskInsertView(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy("usuarios.login")
     model = Task
     form_class = TaskModelForm
     context_object_name = "task"
@@ -51,7 +57,8 @@ class TaskInsertView(CreateView):
         return reverse("tasks.list")
 
 
-class ListTasksView(TemplateView):
+class ListTasksView(LoginRequiredMixin, TemplateView):
+    login_url = reverse_lazy("usuarios.login")
     template_name = "tasks/task_lists.html"
 
     def get_context_data(self, **kwargs):
@@ -61,9 +68,12 @@ class ListTasksView(TemplateView):
         user_logged = self.request.user
         if user_logged.is_authenticated:
             context["tasks"] = Task.objects.filter(user=user_logged).order_by("-id")
+        else:
+            messages.warning(self.request, ("Entre para interagir!"))
         return context
 
     def Done_Task(request, task_id):
+        login_url = reverse_lazy("usuarios.login")
         item = Task.objects.get(pk=task_id)
         item.done = not item.done
         item.save()
