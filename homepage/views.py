8b8dc941d9f8b3454410808b6563409ed38be517
Ariel <mrariel2011@hotmail.com>
2021-12-22 00:08:27 -0300

    Filtra User no Homepage

--- homepage/views.py
+++ homepage/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from tasks.models import Task
 from datetime import date, datetime
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def home(request):
-    doneCount = Task.objects.filter(done=True).count
-    notdoneCount = Task.objects.filter(done=False).count
-    todayCount = Task.objects.filter(due_to=date.today()).count
-    expiredCount = Task.objects.filter(due_to=datetime.now()).count
+    doneCount = Task.objects.filter(done=True, user=request.user).count
+    notdoneCount = Task.objects.filter(done=False, user=request.user).count
+    todayCount = Task.objects.filter(due_to=date.today(), user=request.user).count
+    expiredCount = Task.objects.filter(due_to=datetime.now(), user=request.user).count
 
     context = {
         "done_count": doneCount,
