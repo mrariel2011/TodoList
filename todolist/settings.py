523d957c775c3e0c7c8c44bec5ecc00244a17bac
Ariel <mrariel2011@hotmail.com>
2022-02-20 22:37:54 -0300

    Corrigindo bad commit do settings

--- todolist/settings.py
+++ todolist/settings.py
@@ -103,8 +103,12 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": config("POSTGRES_DB"),
+        "USER": config("POSTGRES_USER"),
+        "PASSWORD": config("POSTGRES_PASSWORD"),
+        "HOST": config("POSTGRES_HOST"),
+        "PORT": config("POSTGRES_PORT"),
     }
 }
 
